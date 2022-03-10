1b0c01a063c036b692a984010864c52ebb8a09df
1000ward <1000ward23@gmail.com>
2022-03-10 18:35:09 -0500

    hecho

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -144,7 +144,7 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
-  for(var i =0; i < ususario.posts.length;i++){
+  for(var i =0; i > ususario.posts.length;i++){
     suma = suma + usuario.posts[i].likes;
   }
   return suma; 
